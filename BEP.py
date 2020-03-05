486d9ad8f4b70161eab3037bb97fbf241ea7596c
Jesus PerezLlano <jesus.miguel.perez.llano@gmail.com>
2020-03-05 22:46:08 +0100

    Change colors

--- BEP.py
+++ BEP.py
@@ -36,7 +36,7 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
+#import dash_bootstrap_components as dbc
 import plotly.figure_factory as ff
 import plotly.graph_objs as go
 
